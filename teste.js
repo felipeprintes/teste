224138fe96aaab229c17ee01422f5a6910bdd178
Anderson Felipe Gomes Printes <felipe.unilasalle@gmail.com>
2018-12-04 11:09:10 +0000

    teste js

--- teste.js
+++ teste.js
@@ -21,25 +21,4 @@ for(i=0; i<facts.length; i++){
 }
 
  console.log(vet)
- /*
-  Olá, 
-  primeiramente gostaria de agradecer pela oportunidade de participar dessa fase do
-  processo seletivo. 
-  
-  Se me permitem fazer uma auto avaliação sobre meu desempenho no teste, falarei um pouco a seguir
-  
-  Gostei muito do teste, e entendi que o objetivo não era bem avaliar meus skills em Js, especificamente,
-  mas minha lógica de programação e acho q o desafio atendeu bem o propósito. 
-  
-  Com relação ao meu desempennho:
-    como o objetivo era passar os dados atuais que estão no banco, o primeiro requisito seria passar
-    apenas o que é True, essa tarefa foi concluída sem muitos problemas.
-    
-    porém, encontrei dificuldade no momento em que eu precisava guardar um elemento, como endereço 
-    para poder validar se aquele elemento ja pertencia ao vetor ao qual estava guardando meus dados
-    mais atuais. 
-    
-    ['joão', 'endereço', 'rua alice, 10', true],
-    ['joão', 'endereço', 'rua bob, 88', true],
-    -> neste exemplo o endereco 'rua bob' deveria sobrescrevar, porém tive dificuldade
- */
+ 
